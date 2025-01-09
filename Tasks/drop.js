62837fc40b21df5afb21ea74a7b11f6e42aa5e0f
DenysP21 <puchkov.denys@lll.kpi.ua>
2025-01-09 17:05:23 +0200

    Remove empty blocks and formar code indentation and spacing

--- Tasks/drop.js
+++ Tasks/drop.js
@@ -1,23 +1,12 @@
 'use strict';
 
 DroP = (D, ...X) => {
-  T = 100;
   T = Object.keys(D);
-  T.forEach(
-    (_) => {
-      {
-        T = [D, X];
-      }
-      if (X.includes(_) && true == 1) {
+  T.forEach((_) => {
+    if (X.includes(_)) {
         delete D[_];
-        {
-          T = T;
-        }
-      }
-    },
-    ['uno', 'due', 'tre']
-  );
-  T = D;
+   }
+  });
   return D;
 };
 
