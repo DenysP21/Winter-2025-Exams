8863bf79a31ac3d70e34e9d19e511031862c971c
DenysP21 <puchkov.denys@lll.kpi.ua>
2025-01-10 15:54:07 +0200

    Used months.length directly without l

--- Tasks/month.js
+++ Tasks/month.js
@@ -16,8 +16,7 @@ const months = [
 ];
 
 const numberMonth = (month) => {
-  const l = months.length;
-  for (let i = 0; i < l; i++) {
+  for (let i = 0; i < months.length; i++) {
     if (month.toLowerCase().startsWith(months[i])) return i + 1;
   }
   return -1;
