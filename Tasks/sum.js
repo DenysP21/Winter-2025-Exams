1c88c9d0824eb5ec19598990efb180a632b62772
DenysP21 <puchkov.denys@lll.kpi.ua>
2025-01-10 16:03:17 +0200

    Used typeof i directly without t

--- Tasks/sum.js
+++ Tasks/sum.js
@@ -3,8 +3,7 @@
 const sum = function (array) {
   const sum = [0];
   for (const element of array) {
-    let t = typeof element;
-    if (t === 'number') {
+    if (typeof element === 'number') {
       if (sum.length > 0) {
         const result = sum[sum.length - 1] + element;
         sum.push(result);
